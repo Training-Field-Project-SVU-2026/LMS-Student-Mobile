cc107513fd811db1794a33f5b26c74a9dfaaf439
Mayar-Abdelrahim <mayarabdelrahim22@gmail.com>
2026-05-21 10:45:25 +0300

    SVU-61 update size of icons in settings screen

--- lib/features/profile/presentation/screens/settings_screen/widgets/setting_tile.dart
+++ lib/features/profile/presentation/screens/settings_screen/widgets/setting_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lms_student/core/extensions/context_extensions.dart';
+import 'package:lms_student/core/utils/get_responsive_size.dart';
 
 class SettingsTile extends StatelessWidget {
   final IconData icon;
@@ -24,21 +25,42 @@ class SettingsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double iconSize = context.responsiveValue(
+      mobile: 22.sp,
+      tablet: 18,
+      desktop: 20,
+    );
+    final double containerPadding = context.responsiveValue(
+      mobile: 10.r,
+      tablet: 7,
+      desktop: 8,
+    );
+    final double containerRadius = context.responsiveValue(
+      mobile: 12.r,
+      tablet: 9,
+      desktop: 10,
+    );
+    final double trailingIconSize = context.responsiveValue(
+      mobile: 14.sp,
+      tablet: 12,
+      desktop: 13,
+    );
+
     return ListTile(
       onTap: onTap,
       contentPadding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
       leading: Container(
-        padding: EdgeInsets.all(10.r),
+        padding: EdgeInsets.all(containerPadding),
         decoration: BoxDecoration(
           color: (iconColor ?? context.colorScheme.primary).withValues(
             alpha: 0.1,
           ),
-          borderRadius: BorderRadius.circular(12.r),
+          borderRadius: BorderRadius.circular(containerRadius),
         ),
         child: Icon(
           icon,
           color: iconColor ?? context.colorScheme.primary,
-          size: 22.sp,
+          size: iconSize,
         ),
       ),
       title: Text(
@@ -61,7 +83,7 @@ class SettingsTile extends StatelessWidget {
           (onTap != null
               ? Icon(
                   Icons.arrow_forward_ios,
-                  size: 14.sp,
+                  size: trailingIconSize,
                   color: context.colorScheme.onSurfaceVariant,
                 )
               : null),
